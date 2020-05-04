0cb707f9204fa2299e7114c45cc33995926fa256
Nicolas Carrasco <nico.carrasco.arandia@outlook.com>
2020-05-04 00:23:52 -0400

    compresor terminado

--- zip.js
+++ zip.js
@@ -10,14 +10,47 @@ function RepeatedChar(cadena, caracter){
     return count;
 }
 
-var diccionario= [];
+var diccionario= {};
+var keyValues = [];
 
-
-for(var i=0;i<cadenaEjemplo.length;i++){
-    if(!diccionario.includes(cadenaEjemplo[i])){
+for(var i=0;i<cadenaEjemplo.length; i++){
+    if(!(cadenaEjemplo[i] in diccionario)){
         var numberOfChar= RepeatedChar(cadenaEjemplo,cadenaEjemplo[i]);
-        diccionario.push(cadenaEjemplo[i]);
-        console.log(cadenaEjemplo[i]+" "+numberOfChar);
+        diccionario[cadenaEjemplo[i]]=numberOfChar;
+        
     }
 
 }
+
+for(var key in diccionario){
+    keyValues.push([key,diccionario[key]]);
+}
+
+keyValues.sort(function(first,second){
+    return second[1] - first[1];
+});
+
+var nbits = keyValues.length
+
+for(var i=keyValues.length-1; i>=0; i--){
+    var codif = ""
+    for(var j=nbits; j>1; j--){
+        codif += "0"
+    }
+    nbits -= 1
+    codif += "1"
+    keyValues[i].push(codif)
+}
+
+console.log(keyValues);
+var salidaCodificada= "";
+var firstBit= "1";
+var salidaConcat = "";
+//armado de la salida
+for(var i=0; i<keyValues.length; i++){
+    for(var j=0; j<keyValues[i][1]; j++){
+        salidaConcat += keyValues[i][2]
+    }
+}
+
+console.log("Salida Codificada: "+salidaConcat);
